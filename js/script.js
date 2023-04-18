fe74d1affc83a8420538e753cd0e32e9be3e9cec
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-04-18 10:14:49 +0200

    fix(js):console log

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const myLink = document.getElementById('myLink');
 myLink.addEventListener('click', (event) => {
     event.preventDefault(); // Prevent the default behavior of following the link
     location.reload(); // Reload the page
+    alert("Play with FizzBuzz!");
 });
 
 // // function to run when button is clicked
@@ -17,21 +18,28 @@ document.getElementById("btn").addEventListener("click", function() {
     // Create a loop that will run 100 times
     for (var i = 1; i <= 100; i++) {
         
+        let myArray = [i];
+        console.log("Value is" , myArray);
+
         // Check if the number is a multiple of 3 and 5
         if (i % 3 == 0 && i % 5 == 0) {
             cont.innerHTML += `<div class="myBox myFirstBg">fizzbuzz</div>`;
+            console.log("This number is fizzbuzz");
         }
         // Check if the number is a multiple of 3
         else if (i % 3 == 0) {
             cont.innerHTML += `<div class="myBox mySecondBg">fizz</div>`;
+            console.log("This number is fizz");
         }
         // Check if the number is a multiple of 5
         else if (i % 5 == 0) {
             cont.innerHTML += `<div class="myBox myThirdBg">buzz</div>`;
+            console.log("This number is buzz");
         } 
         // If the number is not a multiple of 3 or 5
         else {
             cont.innerHTML += `<div class="myBox myFourthBg">${i}</div>`;
+            console.log("This number is",i);
         }
     }}
 );
